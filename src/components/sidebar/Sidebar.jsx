af49f50302c3f7605c994b55087a32c59e55f680
Cao Van Quy <quy.187pm20553@vanlanguni.vn>
2022-06-09 12:10:42 +0700

    Setup Project and done sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,87 @@
+import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import StoreIcon from '@mui/icons-material/Store';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <div className="logo">TobiAdmin</div>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <DashboardIcon className="icon"/>
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <li>
+                        <PersonOutlineOutlinedIcon className="icon"/>
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <StoreIcon className="icon"/>
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <AddShoppingCartIcon className="icon"/>
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <DeliveryDiningIcon className="icon"/>
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <QueryStatsIcon className="icon"/>
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsIcon className="icon"/>
+                        <span>Notifications</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <HealthAndSafetyIcon className="icon"/>
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyIcon className="icon"/>
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsIcon className="icon"/>
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountCircleIcon className="icon"/>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <ExitToAppIcon className="icon"/>
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <div className="colorOption"></div>
+                <div className="colorOption"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
